03f969dfaa443d3c92e399545295423bddb74d4e
MelvinAguilar <00067621@uca.edu.sv>
2022-11-07 16:35:58 -0600

    :sparkles: Add react client default structure

---
+++ Client/travel-go-client/src/App.jsx
@@ -0,0 +1,11 @@
+import classes from './App.module.scss';
+
+function App() {
+  return (
+    <div className={classes["App"]}>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ Client/travel-go-client/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import classes from "./Header.module.scss";
+
+const Header = () => {
+  return (
+    <header className={classes["Header"]}>
+      {/* header */}
+    </header>
+  );
+};
+
+export default Header;

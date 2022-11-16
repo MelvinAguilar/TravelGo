85ceac943e81afbea3feeb92ad524e7b5341c47d
HenryLima07 <00033721@uca.edu.sv>
2022-11-15 18:15:04 -0600

    add some changes on app file

--- Server/travel_go_api/app.js
+++ Server/travel_go_api/app.js
@@ -13,7 +13,7 @@ var app = express();
 
 mongoose.connect();
 
-app.use(cors({}));
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

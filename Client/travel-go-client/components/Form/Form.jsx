044a56883648ad9fecf0a7454d7a485c4a3bd8e6
Henry_Lima <90475134+HenryLima07@users.noreply.github.com>
2022-11-24 14:23:38 -0600

    :fire: Delete trash files

--- Client/travel-go-client/components/Form/Form.jsx
+++
@@ -1,7 +0,0 @@
-import classes from "./Form.module.scss";
-
-const Form = ({ children, className, ...props }) => {
-  return <form className={`${classes["Form"]} ${className}`} {...props}>{children}</form>;
-};
-
-export default Form;

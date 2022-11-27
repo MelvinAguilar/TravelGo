1f2105df56641ba2fa082e0f5694cdabd63d505e
HenryLima07 <00033721@uca.edu.sv>
2022-11-26 18:35:40 -0600

    change comments schema

--- Server/travel_go_api/models/comments.model.js
+++ Server/travel_go_api/models/comments.model.js
@@ -2,19 +2,12 @@ const Mongoose = require("mongoose");
 const {default: mongoose} = require("mongoose");
 const Schema = Mongoose.Schema;
 
-const commentSchema = new Schema({
+const item = new Schema({
     user:{
         type: Schema.Types.ObjectId,
         ref: "User",
         required: true,
         trim: true,
-    },
-    lugar: {
-        type: Schema.Types.ObjectId,
-        ref: "TuristicPlace",
-        required: true,
-        trim: true,
-
     },
     comentario:{
         type: String,
@@ -30,6 +23,22 @@ const commentSchema = new Schema({
         type: Date,
         default: Date.now()
     }
+});
+
+const commentSchema = new Schema({
+
+    lugar: {
+        type: Schema.Types.ObjectId,
+        ref: "TuristicPlace",
+        required: true,
+        trim: true,
+
+    },
+    item:{
+        type: [item],
+        required: true, 
+    }
+    
 });
 
 module.exports = mongoose.model("Comment", commentSchema);

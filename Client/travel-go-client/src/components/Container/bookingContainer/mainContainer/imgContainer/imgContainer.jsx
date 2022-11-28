3f5f7fcb45f9a9b9e548a69f30d71c534053b1db
MelvinAguilar <00067621@uca.edu.sv>
2022-11-28 00:41:22 -0600

    :lipstick: Add zoom component to images

--- Client/travel-go-client/src/components/Container/bookingContainer/mainContainer/imgContainer/imgContainer.jsx
+++ Client/travel-go-client/src/components/Container/bookingContainer/mainContainer/imgContainer/imgContainer.jsx
@@ -1,14 +1,18 @@
 import classes from "./imgContainer.module.scss";
+import Zoom from 'react-medium-image-zoom'
+import 'react-medium-image-zoom/dist/styles.css'
 
 const ImgContainer = ({urlImgs = []})=>{
     const mappedImg = urlImgs.map((urlImg, counter)=>{
-        return(
-            <img 
-                src={urlImg} 
-                alt="Imagen de lugar turistico"
-                key={counter}
-                className={classes[`img${counter}`]}
-            />
+        return (
+            <div key={counter} className={`${classes["ImgContainer__content"]} ${classes[`img${counter}`]}`}>
+            <Zoom>
+                <img 
+                    src={urlImg} 
+                    alt="Imagen de lugar turistico"
+                />
+            </Zoom>
+            </div>
         );
     });
 

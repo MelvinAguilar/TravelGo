130adec500862a8712fc1a72245c130ed7658702
MelvinAguilar <00067621@uca.edu.sv>
2022-11-28 11:51:41 -0600

    :sparkles: Add favorite trips view

--- Client/travel-go-client/src/views/TripsView/TripsView.jsx
+++ Client/travel-go-client/src/views/TripsView/TripsView.jsx
@@ -3,44 +3,46 @@ import Header from "../../components/Header/Header";
 import FooterAttribution from "../../components/Footer/FooterAttribution/FooterAttribution";
 import ExploreContainer from "../../components/Container/ExploreContainer/ExploreContainer";
 import Button from "../../components/Button/Button";
-import Place from "../../components/Trips/Places/Places";
-import axios from 'axios';
 import { Binoculars, HeartFill } from "react-bootstrap-icons";
-import { useState, useEffect } from 'react';
-import { toast } from 'react-toastify';
-import { useConfigContext } from '../../contexts/ConfigContext';
+import { Route, Routes } from "react-router-dom";
+
+import ButtonLink from "../../components/Button/ButtonLink/ButtonLink";
+import FavoriteTripsView from "../FavoriteTripsView/FavoriteTripsView";
+import AllTripsView from "../AllTripsView/AllTripsView";
 
 const TripsView = ({ className, ...rest }) => {
-  
-  const [ trips, setTrips ] = useState([]);
-  const { startLoading, stopLoading } = useConfigContext();
-
-  useEffect(() => {
-    fetchTrips();
-  }, []);
-
-
-  const fetchTrips = async () => {
-    try {
-      startLoading();
-
-      const { data } = await axios.get("/places_info");
-      setTrips(data);
-    } catch (error) {
-      toast.error(error.message, {
-        toastId: "fetchTripsError"
-      });
-    } finally {
-      stopLoading();
-    }
-  };
-
   return (
     <>
       <Header />
       <main className={classes["TripsView"]}>
-      <ExploreContainer>
-          <h2>Trips made with <span><HeartFill/></span>just for you</h2>
+        <ul>
+          <li>
+            <ButtonLink
+              to="/trips/favorite"
+              className={classes["TripsView__button"]}
+            >
+              Favorite
+            </ButtonLink>
+          </li>
+          <li>
+            <ButtonLink to="" className={classes["TripsView__button"]}>
+              Home
+            </ButtonLink>
+          </li>
+        </ul>
+        <Routes>
+          <Route path="/" element={<AllTripsView />} />
+          <Route path="favorite" element={<FavoriteTripsView />} />
+        </Routes>
+
+        <ExploreContainer>
+          <h2>
+            Trips made with{" "}
+            <span>
+              <HeartFill className={classes["TripsView__heart"]} fill="#FF5A5F" />
+            </span>{" "}
+            just for you
+          </h2>
 
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vitae
@@ -50,12 +52,9 @@ const TripsView = ({ className, ...rest }) => {
           </p>
           <Button modifierClass="Button--purple">
             Explore a hidden trip
-            <Binoculars/>
+            <Binoculars />
           </Button>
         </ExploreContainer>
-
-        <Place trips={trips} />
-        
       </main>
       <FooterAttribution />
     </>

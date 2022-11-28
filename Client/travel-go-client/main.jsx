a9fec446e3efa37b1e05d445c371152c871b2069
Fernanda Vásquez <82715400+cam-vasquez@users.noreply.github.com>
2022-11-27 23:25:32 -0600

    :sparkles: Updating

---
+++ Client/travel-go-client/main.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AppComponent from "./App";
+import axios from 'axios';
+import Loader from "./components/Loader/Loader";
+import { ConfigProvider } from "./contexts/ConfigContext";
+import { ToastContainer } from "react-toastify";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+
+import "react-toastify/dist/ReactToastify.css";
+
+axios.defaults.baseURL = import.meta.env.VITE_API || "http://localhost:3500/api";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ConfigProvider>
+        <AppComponent />
+        <ToastContainer
+          theme="light"
+          position="top-right"
+          pauseOnHover="false"
+        />
+        <Loader />
+      </ConfigProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
